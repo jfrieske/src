a1bfc98d4c72abd778a9118d99778e3b1430e04a
szefo <jfrieske@poczta.onet.pl>
2019-10-29 22:26:39 +0100

    66

--- DictionaryActivity.cs
+++ DictionaryActivity.cs
@@ -22,6 +22,35 @@ namespace vocab_tester
 
             FindViewById<Button>(Resource.Id.btnClose).Click += BtnClose_Click;
             FindViewById<Button>(Resource.Id.btnCategoryNew).Click += BtnCategoryNew_Click;
+            FindViewById<Button>(Resource.Id.btnPobierz).Click += async delegate
+            {
+                await KoloAsync();
+            };
+        }
+
+        private async System.Threading.Tasks.Task KoloAsync()
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                // send a GET request  
+                var uri = "http://blog.xamarin.com/feed";
+                var result = await client.GetStreamAsync(uri);
+
+                //handling the answer  
+                //var serializer = new XmlSerializer(typeof(Rss));
+                //var feed = (Rss)serializer.Deserialize(result);
+
+                // generate the output  
+                //var item = feed.Channel.Items.First();
+                //textView.Text = "First Item:\n\n" + item;
+                Toast.MakeText(this, "zrobione", ToastLength.Long).Show();
+            }
+        }
+
+        private void BtnPobierz_Click(object sender, EventArgs e)
+        {
+            DictionaryHttp dictionary = new DictionaryHttp();
+            dictionary.Download();
         }
 
         private void BtnCategoryNew_Click(object sender, EventArgs e)
