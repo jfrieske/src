3064c95db97821332bd192e8498ba590be56cddf
szefo <jfrieske@poczta.onet.pl>
2019-11-11 00:29:24 +0100

    4

--- TestParamsActivity.cs
+++ TestParamsActivity.cs
@@ -17,21 +17,32 @@ namespace vocab_tester
     public class TestParamsActivity : Activity
     {
         private NumberPicker npQuestions;
+        private NumberPicker npAnswers;
         private ISharedPreferences prefs;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_test_params);
 
-            npQuestions = (NumberPicker) FindViewById(Resource.Id.npQuestion);
-            npQuestions.MinValue = 1;
-            npQuestions.MaxValue = 99;
-
             prefs = PreferenceManager.GetDefaultSharedPreferences(ApplicationContext);
+            npQuestions = (NumberPicker) FindViewById(Resource.Id.npQuestions);
+            npQuestions.MinValue = 1;
+            npQuestions.MaxValue = 99;            
             npQuestions.Value = prefs.GetInt("npQuestions_Value", 10);
+            npAnswers = (NumberPicker)FindViewById(Resource.Id.npAnswers);
+            npAnswers.MinValue = 1;
+            npAnswers.MaxValue = 5;
+            npAnswers.Value = prefs.GetInt("npAnswers_Value", 3);
 
             FindViewById<Button>(Resource.Id.btnClose).Click += BtnClose_Click;
             FindViewById<Button>(Resource.Id.btnTest).Click += BtnTest_Click;
+            FindViewById<CheckedTextView>(Resource.Id.checkedTextView1).Click += CheckCategory_Click;
+            FindViewById<CheckedTextView>(Resource.Id.checkedTextView2).Click += CheckCategory_Click;
+        }
+
+        private void CheckCategory_Click(object sender, EventArgs e)
+        {
+            ((CheckedTextView)sender).Checked = !((CheckedTextView)sender).Checked;
         }
 
         private void BtnClose_Click(object sender, EventArgs e)
@@ -43,6 +54,7 @@ namespace vocab_tester
         {
             ISharedPreferencesEditor editor = prefs.Edit();
             editor.PutInt("npQuestions_Value", npQuestions.Value);
+            editor.PutInt("npAnswers_Value", npAnswers.Value);
             editor.Commit();
         }
     }
